ed41197b6237d1c0431bbf5232847fa55c7c7ccc
Jorge Salas <jorsalas1120@gmail.com>
2026-08-24 21:25:11 -0600

    creando el manager para la ui del menu inicial

---
+++ Assets/Code/Core/Managers/UIManagers/InitialMenuUIManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using RED.Utility.Singleton;
+
+public class InitialMenuUIManager : Singleton<InitialMenuUIManager>
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
