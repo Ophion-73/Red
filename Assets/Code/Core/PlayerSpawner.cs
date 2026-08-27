b83e11008c5f9b7d66c3d1ebc08984df9c42ba09
Jorge Salas <jorsalas1120@gmail.com>
2026-08-27 01:03:33 -0600

    ta se spawnea el player

--- Assets/Code/Core/PlayerSpawner.cs
+++ Assets/Code/Core/PlayerSpawner.cs
@@ -2,5 +2,35 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
+    [Header("Player Settings")]
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Transform spawnPoint;
     
+    private GameObject currentPlayerInstance;
+
+    private void OnEnable()
+    {
+        GameEvents.OnPlayerSpawn += HandlePlayerSpawn;
+    }
+    
+    private void OnDisable()
+    {
+        GameEvents.OnPlayerSpawn -= HandlePlayerSpawn;
+    }
+
+    private void HandlePlayerSpawn()
+    {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("No hay un player asignado");
+            return;
+        }
+        
+        Transform targetSpawn = spawnPoint != null ? spawnPoint : transform;
+        
+        if (currentPlayerInstance != null) Destroy(currentPlayerInstance);
+        
+        currentPlayerInstance = Instantiate(playerPrefab, targetSpawn.position, targetSpawn.rotation);
+        Debug.Log("<color=green><b>[PlayerSpawner]</b> Jugador instanciado exitosamente.</color>");
+    }
 }
